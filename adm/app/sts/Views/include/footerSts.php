d717e1c2b6c9ea506d4ed52c33af258cf416fbab
Edenilson João Pereira <ninocriptocoin@gmail.com>
2023-01-25 23:44:10 -0300

    Criação do JS no pacote sts

--- adm/app/sts/Views/include/footerSts.php
+++ adm/app/sts/Views/include/footerSts.php
@@ -4,10 +4,11 @@ if(!defined('$2y!10#OaHjLtRhiDTKNv(2022)TkYurzF')){ header("Location: https://lo
 <div class="container-fluid pt-2">
     <div class="row">
         <div class="col-12 text-center">
-            <small>Curso CELKE PHPDevelopment - Modulo PHP POO MVC - 14 Novembro 2022 &copy;</small>
+            <small>STS: Curso CELKE PHPDevelopment - Modulo PHP POO MVC - 14 Novembro 2022 &copy;</small>
         </div>
     </div>
 </div>
+<script src="<?=URLADM?>app/sts/assets/js/custom_sts.js"></script>
 <script src="<?=URLADM?>app/adms/assets/js/custom_adms.js"></script>
 <script src="<?=URLADM?>app/adms/assets/js/adms_layout.js"></script>
 </body>
